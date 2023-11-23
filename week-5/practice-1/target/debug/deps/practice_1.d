545f895aaa6a7dde09d5ac9f3844616687e6c0b1
Aumarsadiq <147741448+Aumarsadiq@users.noreply.github.com>
2023-11-23 14:57:34 +0100

    Delete week-5 directory

--- week-5/practice-1/target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\EXAMS\a.umarsadiqCOS101\week-5\practice-1\target\debug\deps\practice_1.exe: src\main.rs
-
-C:\Users\EXAMS\a.umarsadiqCOS101\week-5\practice-1\target\debug\deps\practice_1.d: src\main.rs
-
-src\main.rs:
--- week-5/practice-1/target/debug/practice-1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\EXAMS\a.umarsadiqCOS101\week-5\practice-1\target\debug\practice-1.exe: C:\Users\EXAMS\a.umarsadiqCOS101\week-5\practice-1\src\main.rs
