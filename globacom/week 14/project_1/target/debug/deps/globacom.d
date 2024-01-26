0867bf471e2d43386df3d08590c2f767146a5245
Aumarsadiq <umarsadiqalamin@gmail.com>
2024-01-26 22:56:20 +0100

    (My Week 14th Commit)

---
+++ globacom/week 14/project_1/target/debug/deps/globacom.d
@@ -0,0 +1,5 @@
+C:\Users\umars\a.umarsadiqCOS101\globacom\week 14\project_1\target\debug\deps\globacom.exe: src\main.rs
+
+C:\Users\umars\a.umarsadiqCOS101\globacom\week 14\project_1\target\debug\deps\globacom.d: src\main.rs
+
+src\main.rs:
---
+++ globacom/week 14/project_1/target/debug/globacom.d
@@ -0,0 +1,1 @@
+C:\Users\umars\a.umarsadiqCOS101\globacom\week\ 14\project_1\target\debug\globacom.exe: C:\Users\umars\a.umarsadiqCOS101\globacom\week\ 14\project_1\src\main.rs
